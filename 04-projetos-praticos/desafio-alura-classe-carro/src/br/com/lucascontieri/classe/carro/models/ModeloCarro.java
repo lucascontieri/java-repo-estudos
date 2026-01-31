eef41a6aa3a73d66b0f6fb197cb2dac3afb5de04
lucascontieri <lucascontierifilho@gmail.com>
2026-01-31 15:44:37 -0300

    initial commit

---
+++ 04-projetos-praticos/desafio-alura-classe-carro/src/br/com/lucascontieri/classe/carro/models/ModeloCarro.java
@@ -0,0 +1,11 @@
+package br.com.lucascontieri.classe.carro.models;
+
+public class ModeloCarro extends Carro {
+    private String corCarro;
+
+    public void setCorCarro(String corCarro) {
+        this.corCarro = corCarro;
+    }
+}
+
+
